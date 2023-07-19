b83b949df6a729f6f21d72987d6b37d44aa36672
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-07-19 18:55:14 +0600

    added new house added route

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
+  }
+  // bearer token
+  const token = authorization.split(' ')[1]
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+
 // ========================================================================>
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d9zindd.mongodb.net/?retryWrites=true&w=majority`;
@@ -95,7 +114,7 @@ async function run() {
       const passwordMatch = await bcrypt.compare(password, user.password);
 
       if (!passwordMatch) {
-        return res.status(401).send({message: 'Invalid password'});
+        return res.status(401).send({ message: 'Invalid password' });
       }
 
       // Generate a new JWT
@@ -109,6 +128,47 @@ async function run() {
 
       res.send({ result, email, token });
     });
+
+    app.get('/house-owner', verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      if (req.decoded.email !== email) {
+        res.send({ houseOwner: false })
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const result = { houseOwner: user?.role === "House Owner" }
+      res.send(result);
+    });
+
+    app.get('/get-role', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get('/my-houses', verifyJWT, async (req, res) => {
+      const email = req.query.email;
+
+      if (!email) {
+        res.send([]);
+      }
+
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'forbidden access' })
+      }
+
+      const query = { owener_email: email };
+      const result = await houseCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/houses', verifyJWT, async (req, res) => {
+      const newHouse = req.body;
+      const result = await houseCollection.insertOne(newHouse);
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
